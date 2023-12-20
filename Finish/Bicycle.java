3da36dcc1ac9c6eb294c68c6f4715ab931c0ca7d
mark-feodosiy <mark-feodosiy@yandex.ru>
2023-12-20 22:20:58 +0300

    Домашка ООП2

---
+++ Finish/Bicycle.java
@@ -0,0 +1,31 @@
+package Finish;
+
+public class Bicycle {
+    public String modelName;
+    public int wheelsCount;
+
+    public Bicycle(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
+    }
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}
---
+++ Finish/Truck.java
@@ -0,0 +1,40 @@
+package Finish;
+
+public class Truck {
+    public String modelName;
+    public int wheelsCount;
+
+    public Truck(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
+    }
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+
+    public void checkTrailer() {
+        System.out.println("Проверяем прицеп");
+    }
+
+}
